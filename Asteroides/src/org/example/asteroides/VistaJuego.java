2e0d90921db4653a4b914ca9992222a484972fd4
Rafa Garrido <rgarom@gmail.com>
2013-03-03 23:54:25 +0100

    Movimiento Nave

--- Asteroides/src/org/example/asteroides/VistaJuego.java
+++ Asteroides/src/org/example/asteroides/VistaJuego.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class VistaJuego extends View {
@@ -24,6 +25,17 @@ public class VistaJuego extends View {
     private Vector<Grafico> Asteroides; // Vector con los Asteroides
     private int numAsteroides = 5; // Número inicial de asteroides
     private int numFragmentos = 3; // Fragmentos en que se divide
+
+    // //// THREAD Y TIEMPO //////
+    // Thread encargado de procesar el juego
+    private ThreadJuego thread = new ThreadJuego();
+    // Cada cuanto queremos procesar cambios (ms)
+    private static int PERIODO_PROCESO = 50;
+    // Cuando se realiz— el œltimo proceso
+    private long ultimoProceso = 0;
+
+    private float mX = 0, mY = 0;
+    private boolean disparo = false;
 
     public VistaJuego(Context context, AttributeSet attrs) {
 
@@ -47,8 +59,8 @@ public class VistaJuego extends View {
     }
 
     @Override
-    protected void onSizeChanged(int ancho, int alto, int ancho_anter,
-	    int alto_anter) {
+    synchronized protected void onSizeChanged(int ancho, int alto,
+	    int ancho_anter, int alto_anter) {
 
 	super.onSizeChanged(ancho, alto, ancho_anter, alto_anter);
 
@@ -63,15 +75,88 @@ public class VistaJuego extends View {
 
 	nave.setPosX(ancho / 2);
 	nave.setPosY(alto / 2);
+
+	ultimoProceso = System.currentTimeMillis();
+	thread.start();
     }
 
     @Override
-    protected void onDraw(Canvas canvas) {
+    synchronized protected void onDraw(Canvas canvas) {
 	super.onDraw(canvas);
 	for (Grafico asteroide : Asteroides) {
 	    asteroide.dibujaGrafico(canvas);
 	}
 
 	nave.dibujaGrafico(canvas);
+    }
+
+    protected void actualizaFisica() {
+	long ahora = System.currentTimeMillis();
+	// No hagas nada si el per’odo de proceso no se ha cumplido.
+	if (ultimoProceso + PERIODO_PROCESO > ahora) {
+	    return;
+	}
+	// Para una ejecuci—n en tiempo real calculamos retardo
+	double retardo = (ahora - ultimoProceso) / PERIODO_PROCESO;
+	ultimoProceso = ahora; // Para la pr—xima vez
+	// Actualizamos velocidad y direcci—n de la nave a partir de
+	// giroNave y aceleracionNave (segœn la entrada del jugador)
+	nave.setAngulo((int) (nave.getAngulo() + giroNave * retardo));
+	double nIncX = nave.getIncX() + aceleracionNave
+		* Math.cos(Math.toRadians(nave.getAngulo())) * retardo;
+	double nIncY = nave.getIncY() + aceleracionNave
+		* Math.sin(Math.toRadians(nave.getAngulo())) * retardo;
+	// Actualizamos si el m—dulo de la velocidad no excede el m‡ximo
+	if (Math.hypot(nIncX, nIncY) <= Grafico.getMaxVelocidad()) {
+	    nave.setIncX(nIncX);
+	    nave.setIncY(nIncY);
+	}
+	// Actualizamos posiciones X e Y
+	nave.incrementaPos(retardo);
+	for (Grafico asteroide : Asteroides) {
+	    asteroide.incrementaPos(retardo);
+	}
+    }
+
+    class ThreadJuego extends Thread {
+	@Override
+	public void run() {
+	    while (true) {
+		actualizaFisica();
+	    }
+	}
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+	super.onTouchEvent(event);
+	float x = event.getX();
+	float y = event.getY();
+	switch (event.getAction()) {
+	case MotionEvent.ACTION_DOWN:
+	    disparo = true;
+	    break;
+	case MotionEvent.ACTION_MOVE:
+	    float dx = Math.abs(x - mX);
+	    float dy = Math.abs(y - mY);
+	    if (dy < 6 && dx > 6) {
+		giroNave = Math.round((x - mX) / 2);
+		disparo = false;
+	    } else if (dx < 6 && dy > 6) {
+		aceleracionNave = Math.round((mY - y) / 25);
+		disparo = false;
+	    }
+	    break;
+	case MotionEvent.ACTION_UP:
+	    giroNave = 0;
+	    aceleracionNave = 0;
+	    if (disparo) {
+		//ActivaMisil();
+	    }
+	    break;
+	}
+	mX = x;
+	mY = y;
+	return true;
     }
 }
